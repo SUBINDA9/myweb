ab59b9b6868bf4868db1776c48ea73adeb76d128
SUBINDA RANA <44157913+SUBINDA9@users.noreply.github.com>
2020-12-22 09:23:33 +0530

    Update server.js
    port code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 7777;
+var port = process.env.PORT || 0909;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
